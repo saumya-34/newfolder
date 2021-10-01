a60b9110b8c9c75ec386829972923d894afadcf7
saumya-34 <90781983+saumya-34@users.noreply.github.com>
2021-10-01 08:59:39 +0530

    This is first Assignment

---
+++ Assignment.js
@@ -0,0 +1,142 @@
+//This is Que 1
+// This is first JavaScript Project for batch FS21AUG01
+/* This Project 
+will deal with 
+the basics of JavaScript*/
+
+//This is Que 2
+var myName = "JavaScript";
+console.log("The variablw myName is ",myName);
+//This Que 3
+var a = 7;
+console.log("The variable a is ",a);
+//This is Que 4
+var b = a;
+console.log("The variable b is ",b);
+//This is Que 5
+a = 9;
+console.log("The variable a has been changes to ",a);
+//This is Que 6
+var a1=5, b1=10, c1="I am a";
+console.log("The variables a1, b1, c1 have been assigned the following values ",a1,b1,c1);
+//This is Que 7
+var thisIsDemoVariable;
+
+//This is Que 8
+var add = a1 + b1;
+console.log("The sum of a1 and b1 is ",add);
+
+//This is Que 9
+var sub = b1-a1;
+console.log("The difference of a1 and b1 is ",sub);
+//This is Que 10
+var mul = a1 * b1;
+console.log("The multiplication of two numbers is ",mul);
+//This Que 11;
+var div = b1/a1;
+console.log("b1 divided by a1 is ",div);
+//This is Que 12
+var i = 0;
+i++;
+console.log("The variable i has been incremented from 0 to ",i);
+//This is Que 13
+var j = 1;
+j--;
+console.log("The variable j has been decremented from 1 to ",j);
+//This is Que 14
+var myDecimal = 5.7
+console.log("My decimal variable is ",myDecimal);
+//This is Que 15
+var decimalMul = 5.7*2.2;
+console.log("Decimal multiplication: ",decimalMul);
+//This is Que 16
+var decimalDiv = 2.2/1.1;
+console.log("Decimal division: ",decimalDiv);
+//This is Que 17
+var remainder = 11%3;
+console.log("The remainder of 11 divided by 3 is ",remainder);
+//This is Que 18
+a+=2;//a=9
+b+=2;//b=7
+var c = 11;
+c+=2;
+console.log("The variables a,b,c after the compound addition are ",a,b,c);
+//This is que 19
+a-=5;
+b-=5;
+c-=5;
+console.log("The varibales a,b,c after the compound subtraction are ",a,b,c);
+//This is Que 20
+a*=3;
+b*=3;
+c*=3;
+console.log("The variables a,b,c after the compound multiplication are ",a,b,c);
+//This is Que 21
+a/=3;
+b/=3;
+c/=3;
+console.log("The variables a,b,c after the compound division are ",a,b,c);
+//This is que 22
+var myFirstName = "Jane";
+var myLastName = "Goodall";
+console.log("My name is ", myFirstName +" "+ myLastName);
+//This is que 23
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."
+console.log(myStr);
+//This is 24
+var conversation = 'Finn exclaims to Jake, "Algebraic!"';
+console.log("Using single quotes ",conversation);
+//This is que 25
+var escapeSequences = 'FirstLine\n\t\\SecondLine\nThirdLine';
+console.log("The esacape sequences were used in the string\n",escapeSequences);
+//This is que 26
+var myString = "This is the start " + "This is the end";
+console.log("The concatenation operation was used for the string ",myString);
+//This is que 27
+var ourStr = "This is the first String ";
+ourStr += "This is the second String";
+console.log("This string was created using += operator ", ourStr);
+//This is que 28
+var myName = "Jane";
+console.log("My name is " + myName + ". I am well");
+//This is que 29
+var myAdjective = "awesome!";
+var myNoun = "freecodecamp is ";
+console.log(myNoun+=myAdjective);
+//This is que 30
+var firstLetterOfLastName = myLastName[0];
+console.log("The first letter of last name is ",firstLetterOfLastName);
+//This is que 31
+var lastNameLength = myLastName.length;
+console.log("The length of Last Name is ", lastNameLength);
+//This is que 32
+var myString = "Job";
+myString = "Hello World!";
+console.log("Immutable String has been changed to ", myString);
+//This is que 33
+var thirdLetterOfLastName = myLastName[2];
+console.log("This is the third letter of my last name ",thirdLetterOfLastName);
+//This is que 34
+var lastLetter = myLastName[myLastName.length-1];
+console.log("The last letter of my last name is ", lastLetter);
+//This is que 35
+var secondLastCharacter = myLastName[myLastName.length-2];
+console.log("Second-to-last character of last name is ",secondLastCharacter);
+//This is que 36
+var ourAdj = "awkward";
+var ourVerb = "amused";
+var ourNoun = "with dumb charades";
+console.log('This is the madlibs game:\n\tIt was really ',ourAdj + ', and we ' + ourVerb + ' ourselves ' + ourNoun);
+//This is que 37
+var array = ["fruits",12];
+console.log("Print the array ", array);
+//This is que 38
+var myArray = [["Books",11],["Fruits",12],["Pens",13]];
+console.log("My nested arry looks like this ",myArray);
+//This is que 39
+var arr = [10,20,30,40];
+var myData = arr[2];
+console.log("The third element of my array is ", arr[2]);
+//This is que 40
+arr[0]=45;
+console.log("The 0th element has been changed from 10 to ",arr[0]);
